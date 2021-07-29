fc5c027a338186fa1deb7138e3889a0111fa42dd
Sound Development <info@sounddevelopment.nl>
2021-07-29 14:26:05 +0200

    Refactor. Documentation.

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -17,18 +17,18 @@ SoundMeterExampleAudioProcessorEditor::SoundMeterExampleAudioProcessorEditor (So
 
    // Set the meter OPTONS ...
    sd::SoundMeter::Options meterOptions;
-   meterOptions.faderEnabled          = false;
-   meterOptions.headerEnabled         = false;
-   meterOptions.valueEnabled          = false;
-   meterOptions.refreshRate           = refreshRate_hz;
-   meterOptions.useGradient           = true;
-   meterOptions.showPeakHoldIndicator = false;
-   meterOptions.peakRegion_db         = -3.0f;   // -3.0 dB peak region divider.
-   meterOptions.warningRegion_db      = -12.0f;  // -12.0 dB warning region indicator.
-   meterOptions.tickMarksEnabled      = true;
-   meterOptions.tickMarksOnTop        = true;
+   meterOptions.faderEnabled = false;  // Enable or disable the 'fader' overlay. Use the sd::SoundMeter::MetersComponent::FadersChangeListener to get the fader value updates.
+   meterOptions.headerEnabled         = false;           // Enable the 'header' part above the meter, displaying the channel ID.
+   meterOptions.valueEnabled          = false;           // Enable the 'value' part below the level, displaying the peak level.
+   meterOptions.refreshRate           = refreshRate_hz;  // Frequency of the meter updates (when using the internal timer).
+   meterOptions.useGradient           = true;            // Use gradients to fill the meter (hard segment boundaries otherwise).
+   meterOptions.showPeakHoldIndicator = false;           // Show the peak hold indicator (double click value to reset).
+   meterOptions.peakSegment_db        = -3.0f;           // -3.0 dB peak segment divider.
+   meterOptions.warningSegment_db     = -12.0f;          // -12.0 dB warning segment indicator.
+   meterOptions.tickMarksEnabled      = true;            // Enable tick-marks. Divider lines at certain levels on the meter and label strip.
+   meterOptions.tickMarksOnTop        = true;            // Put the tick-marks above the level readout.
    meterOptions.tickMarks             = { -1.0f, -3.0f, -6.0f, -12.0f, -18.0f, -36.0f };  // Positions (in decibels) of the tick-marks.
-   meterOptions.decayTime_ms          = 1000.0f;
+   meterOptions.decayTime_ms          = 1000.0f;                                          // The meter will take 1000 ms to decay to 0.
    m_inputMeters.setOptions (meterOptions);
 
    // Use (or don't use) the label strip on the side of the meters.
@@ -38,8 +38,10 @@ SoundMeterExampleAudioProcessorEditor::SoundMeterExampleAudioProcessorEditor (So
    // Set the meter CHANNEL FORMAT (determines number of meter and type description)...
    m_inputMeters.setChannelFormat (p.getOutputFormat());
 
+   // Add the meters to the editor and make them visible...
    addAndMakeVisible (m_inputMeters);
 
+   // Setup and add the corner re-sizer...
    m_sizeConstrainer.setSizeLimits (10, 10, 1300, 2000);
    addAndMakeVisible (m_cornerResizer);
 
@@ -47,6 +49,7 @@ SoundMeterExampleAudioProcessorEditor::SoundMeterExampleAudioProcessorEditor (So
    // editor's size to whatever you need it to be.
    setSize (130, 700);
 
+   // Start the 'polling' timer, that checks the audio processor for peak levels...
    startTimerHz (refreshRate_hz);
 }
 
@@ -55,19 +58,19 @@ SoundMeterExampleAudioProcessorEditor::~SoundMeterExampleAudioProcessorEditor() 
 
 void SoundMeterExampleAudioProcessorEditor::resized()
 {
+   // Position the meters...
    m_inputMeters.setBounds (getLocalBounds());
 
+   // Position the corner re-sizer...
    const int cornerResizerSize = 15;
    m_cornerResizer.setBounds (getLocalBounds().removeFromBottom (cornerResizerSize).removeFromRight (cornerResizerSize));
-
-   // This is generally where you'll want to lay out the positions of any
-   // subcomponents in your editor..
 }
 
+// The 'polling' timer.
 void SoundMeterExampleAudioProcessorEditor::timerCallback()
 {
    // Loop through all meters (channels)...
-   for (int meterIndex = 0; meterIndex < m_inputMeters.getNumMeters(); ++meterIndex)
+   for (int meterIndex = 0; meterIndex < m_inputMeters.getNumChannels(); ++meterIndex)
    {
       // Get the level, of the specified meter (channel), from the audio processor...
       m_inputMeters.setInputLevel (meterIndex, m_audioProcessor.getPeakLevel (meterIndex));
